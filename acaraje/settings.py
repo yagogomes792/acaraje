26f16f3224cfebf58e26f6874b3d4be8bc8b8adb
yagogomes792 <yago.792@hotmail.com>
2021-02-24 15:06:44 -0300

    update procfile

--- acaraje/settings.py
+++ acaraje/settings.py
@@ -123,3 +123,5 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+django_heroku.settings(locals())
